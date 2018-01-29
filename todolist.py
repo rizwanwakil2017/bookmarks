e26bff577dfcde8404fc33ca6eaefa017bc957c5
Rizwan Khan <rizwan.office1979@gmail.com>
2018-01-29 16:43:24 +0400

    Create super user account

---
+++ todolist.py
@@ -0,0 +1,21 @@
+#TODO:1.(Complete) creating isolated virtual environment and Installing Django in virtual environment
+#TODO:2.(Complete) create project
+#TODO:3.(Complete) create initial database for project
+#TODO:4.(Complete) test the initial setup by running the development server
+#TODO:5.(Complete) configure the project in IDE (Pycharm)
+#TODO:6.(Complete) creating and registering application in project
+#TODO:7.	Creating model (tables ) in database for application
+#TODO:8.(Complete )creating super user for site
+#TODO:9.	Adding model to Django administration site
+#TODO:10.	Working with queryset and model manager
+#TODO:11.   Creating views for list and detail page
+#TODO:12.   Creating URL entry for list and detail views
+#TODO:13.   Creating base template for website
+#TODO:14.   Creating template for list and detail page based on base template
+#TODO:15.	Make setup for static file like css, javascript , images
+#TODO:16.   Add method to model to ease the url for multiple argument taking views
+#TODO:17.   Creating forms and handling forms
+#TODO:18			create form
+#TODO:19 			create view to handle form
+#TODO:20.	Create template tags ( simple, inclusion, assignment tags)
+#TODO:21		Create directory in app --> templatetags --> create new file in folder --> add following code in it
